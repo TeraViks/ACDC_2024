2e13013dfb9f305dd30c9b8ecf72840d5b3d02a1
SamueljWilson <samspamgbot@gmail.com>
2024-01-19 09:27:56 -0800

    Added functionality for a interpolation table

---
+++ src/main/java/frc/robot/subsystems/ShooterInterp.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.Arrays;
+
+public class ShooterInterp extends SubsystemBase {
+  static class PosEntry{double distance; double power; public PosEntry(double dist, double pow) {distance = dist; power = pow;}}
+  static final PosEntry[] posTable = {};
+  /** Creates a new ShooterInterp. */
+  public ShooterInterp() {}
+
+  public static double getMaxDistance() {
+    return posTable[posTable.length-1].distance;
+  }
+
+  public static int distanceIndex(double distance) {
+    assert(distance >= 0);
+    int index = Arrays.binarySearch(posTable, new PosEntry(distance, 0), (a,b) -> Double.compare(a.distance, b.distance));
+    if (index < 0) {
+      // Imperfect match
+      index = -index - 1;
+    }
+    assert(index >= 0);
+    return index;
+  }
+
+  public static double distanceToPower(double distance) {
+    int index = distanceIndex(distance);
+    if (index == 0) {
+      return posTable[index].power;
+    } else if (index == posTable.length) {
+      return posTable[posTable.length-1].power;
+    } else if (posTable[index].distance == distance) {
+      return posTable[index].power;
+    }
+    // Linear Interpolation
+    else {
+    double d0 = posTable[index-1].distance;
+    double d1 = posTable[index].distance;
+    double p0 = posTable[index-1].power;
+    double p1 = posTable[index].power;
+    double scaler = (distance-d0) / (d1 - d0);
+    double powerRange = p1 - p0;
+    return p0 + scaler*powerRange;
+    }
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
