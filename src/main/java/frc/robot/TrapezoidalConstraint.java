TeraViks/ACDC_2024

commit 04fefe330bdd3b64360d0f5cfd09980e81b2aab2

Fix TrapezoidalConstraint bugs
Reverse accel/decel constraints for double input if velocity is
currently negative.

Clamp maximum velocity for Translation2d input.

src/main/java/frc/robot/TrapezoidalConstraint.java
@@ -18,15 +18,31 @@ public class TrapezoidalConstraint {
   }
 
   public double calculate(double desiredVelocity, double currentVelocity, double deltaT) {
-    double acceleration = MathUtil.clamp((desiredVelocity - currentVelocity) / deltaT, -m_maxDeceleration, m_maxAcceleration);
-    double deltaV = acceleration * deltaT;
-    double newVelocity = currentVelocity + deltaV;
-    return MathUtil.clamp(newVelocity, -m_maxSpeed, m_maxSpeed);
+    double desiredAcceleration = (desiredVelocity - currentVelocity) / deltaT;
+    double maxDeceleration, maxAcceleration;
+    if (currentVelocity >= 0.0) {
+      maxDeceleration = -m_maxDeceleration;
+      maxAcceleration = m_maxAcceleration;
+    } else {
+      maxDeceleration = -m_maxAcceleration;
+      maxAcceleration = m_maxDeceleration;
+    }
+    double clampedAcceleration = MathUtil.clamp(desiredAcceleration, maxDeceleration, maxAcceleration);
+    double deltaV = clampedAcceleration * deltaT;
+    double unclampedNewVelocity = currentVelocity + deltaV;
+    double clampedNewVelocity = MathUtil.clamp(unclampedNewVelocity, -m_maxSpeed, m_maxSpeed);
+    return clampedNewVelocity;
   }
 
   public Translation2d calculateTranslation2d(Translation2d desiredVelocity, Translation2d currentVelocity, double deltaT) {
     if (desiredVelocity.equals(currentVelocity)) {
       return desiredVelocity;
+    }
+    double desiredVelocityMagnitude = desiredVelocity.getNorm();
+    if (desiredVelocityMagnitude != 0.0) {
+      double clampedVelocityMagnitude = MathUtil.clamp(desiredVelocityMagnitude, -m_maxSpeed, m_maxSpeed);
+      double velocityScalar = clampedVelocityMagnitude / desiredVelocityMagnitude;
+      desiredVelocity = desiredVelocity.times(velocityScalar);
     }
     Translation2d desiredDeltaVelocity = desiredVelocity.minus(currentVelocity);
     Translation2d desiredAcceleration = desiredDeltaVelocity.div(deltaT);
@@ -34,6 +50,7 @@ public class TrapezoidalConstraint {
     assert(desiredAccelerationMagnitude != 0.0);
     double clampedAccelerationMagnitude = MathUtil.clamp(desiredAccelerationMagnitude, -m_maxDeceleration, m_maxAcceleration);
     double accelerationScalar = clampedAccelerationMagnitude / desiredAccelerationMagnitude;
-    return currentVelocity.plus(desiredDeltaVelocity.times(accelerationScalar));
+    Translation2d newVelocity = currentVelocity.plus(desiredDeltaVelocity.times(accelerationScalar));
+    return newVelocity;
   }
 }

commit ee188d70e6deb2ba4016876e487cd0c48891e0bd

Fix division by 0 in TrapezoidalConstraint.calculateTranslation2d()
Clamping can reduce the desired velocity to the current velocity, which
isn't detected by the equality check at the beginning of the method. Add
a simple check that the clamped desired acceleration magnitude is
non-zero prior to division; if zero return the current velocity.

This fixes a regression introduced by
04fefe330bdd3b64360d0f5cfd09980e81b2aab2 (Fix TrapezoidalConstraint
bugs).

src/main/java/frc/robot/TrapezoidalConstraint.java
@@ -47,7 +47,9 @@ public class TrapezoidalConstraint {
     Translation2d desiredDeltaVelocity = desiredVelocity.minus(currentVelocity);
     Translation2d desiredAcceleration = desiredDeltaVelocity.div(deltaT);
     double desiredAccelerationMagnitude = desiredAcceleration.getNorm();
-    assert(desiredAccelerationMagnitude != 0.0);
+    if (desiredAccelerationMagnitude == 0.0) {
+      return currentVelocity;
+    }
     double clampedAccelerationMagnitude = MathUtil.clamp(desiredAccelerationMagnitude, -m_maxDeceleration, m_maxAcceleration);
     double accelerationScalar = clampedAccelerationMagnitude / desiredAccelerationMagnitude;
     Translation2d newVelocity = currentVelocity.plus(desiredDeltaVelocity.times(accelerationScalar));