a771e604ac1c91f0a9f232ee701489ac58edc722
teravik3 <teravik3@gmail.com>
2024-03-09 17:20:10 -0800

    Fixing the problem with the autos in commands

--- src/main/java/frc/robot/commands/PickupCommand.java
+++ src/main/java/frc/robot/commands/PickupCommand.java
@@ -6,4 +6,9 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class PickupCommand extends Command {}
+public class PickupCommand extends Command {
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -6,4 +6,9 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class ShooterCommand extends Command {}
+public class ShooterCommand extends Command {
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
