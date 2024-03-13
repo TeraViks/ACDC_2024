92b05c5ac507329b2493dea378f3225cda77172e
Jason Evans <jasone@canonware.com>
2024-03-12 17:09:34 -0700

    Tune note targeting
    The Limelight data are remarkably noisy, so much of this change is
    related to noise reduction. In particular, the median filter size is now
    7 instead of 5, the angle tolerance is now 5 degrees instead of 3, and
    some effort is expended to smooth the effects of spurious note detection
    failures.
    
    Fix a latent bug in note/speaker targeting angle velocity PID
    initialization. Both PIDs were initialized with the I value for the D
    parameter, but since both I and D were set to 0.0, this had no impact on
    behavior.

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -21,8 +21,9 @@ public class Limelight extends SubsystemBase {
   private NetworkTableEntry m_ta = m_table.getEntry("ta");
   private NetworkTableEntry m_tv = m_table.getEntry("tv");
 
-  private MedianFilter m_xFilter = new MedianFilter(5);
-  private MedianFilter m_yFilter = new MedianFilter(5);
+  private int m_nConsecutiveFabrications = 0;
+  private MedianFilter m_xFilter = new MedianFilter(LimelightConstants.kFilterSize);
+  private MedianFilter m_yFilter = new MedianFilter(LimelightConstants.kFilterSize);
   private double m_filteredX = 0.0;
   private double m_filteredY = 0.0;
 
@@ -46,16 +47,32 @@ public class Limelight extends SubsystemBase {
     double y = m_ty.getDouble(0.0);
     double area = m_ta.getDouble(0.0);
     double v = m_tv.getDouble(0.0);
+    boolean notePresent = (v == 1.0);
 
-    m_filteredX = m_xFilter.calculate(x);
-    m_filteredY = m_yFilter.calculate(y);
+    if (notePresent) {
+      m_nConsecutiveFabrications = 0;
+      m_filteredX = m_xFilter.calculate(x);
+      m_filteredY = m_yFilter.calculate(y);
 
+      SmartDashboard.putNumber("LimeLightArea", area);
+      var distance = getDistance();
+      SmartDashboard.putNumber("Distance to Note (in)", distance);
+    } else {
+      if (m_nConsecutiveFabrications < LimelightConstants.kFilterSize) {
+        m_nConsecutiveFabrications++;
+        m_filteredX = m_xFilter.calculate(m_filteredX);
+        m_filteredY = m_yFilter.calculate(m_filteredY);
+      } else {
+        m_nConsecutiveFabrications = 0;
+        m_xFilter.reset();
+        m_yFilter.reset();
+        m_filteredX = 0.0;
+        m_filteredY = 0.0;
+      }
+    }
     SmartDashboard.putNumber("LimeLightX", m_filteredX);
     SmartDashboard.putNumber("LimeLightY", m_filteredY);
-    SmartDashboard.putNumber("LimeLightArea", area);
-    SmartDashboard.putBoolean("Note", v == 1);
-    var distance = getDistance();
-    SmartDashboard.putNumber("Distance to Note (in)", distance);
+    SmartDashboard.putBoolean("Note", notePresent);
   }
 
   public double getX() {
