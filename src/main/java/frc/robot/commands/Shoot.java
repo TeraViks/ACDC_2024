eb1b2708db99a19803cf3d10ce52166a4578473a
Keira P <keira@JASON-WIN8.nuggetmarket.com>
2024-03-16 12:27:09 -0700

    renamed shootercommand to shoot

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -6,7 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class ShooterCommand extends Command {
+public class Shoot extends Command {
   @Override
   public boolean isFinished() {
     return true;
